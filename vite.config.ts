070e59722b497f6df8c722d637b2335ce08185ea
Manjul Tamrakar <manjultamrakar4@gmail.com>
2025-01-03 22:55:43 +0545

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Website/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
