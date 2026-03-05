af987ed4250ea992444484a1d707641696d6c73e
manjul-veelapp <manjul@veelapp.com>
2026-03-05 09:32:17 +0545

    updated resume

--- src/page/project.tsx
+++ src/page/project.tsx
@@ -4,43 +4,19 @@ const items = [
     image: '/sites/phasezero.png',
     link: 'https://phasezeroclothing.com/',
     title: 'Phase Zero Clothing',
-    description: 'Clothing E-commerce Site',
+    description: 'E-commerce Website',
   },
   {
     image: '/sites/BefitNepal.png',
     link: 'https://play.google.com/store/apps/details?id=com.colthinkspace.befitnepal',
     title: 'Befit Nepal & Befit Partner',
-    description: 'Gym Management App',
-  },
-  {
-    image: '/sites/zentabdemo.png',
-    link: 'https://zen-tab.pages.dev/',
-    title: 'Zen Tab',
-    description: 'Productivity Browser Extension',
-  },
-  {
-    image: '/sites/packageforge.png',
-    link: 'https://pkgs.pkgforge.dev/',
-    title: 'Pkgforge Package Index',
-    description: 'Index of Packages under Pkgforge',
+    description: 'Mobile Application',
   },
   {
     image: '/sites/collearn2.png',
     link: 'https://learn.colglobal.org/',
     title: 'COL Learn',
     description: 'Learning Management System',
-  },
-  {
-    image: '/sites/notemark.png',
-    link: 'https://github.com/Tamrakar182/note-mark',
-    title: 'Note Mark',
-    description: 'Desktop application for Note Taking',
-  },
-  {
-    image: '/sites/metroidvania.png',
-    link: 'https://tamrakar182.github.io/metroidvania/',
-    title: 'Void Knight - Silktune',
-    description: 'Metroidvania Game in Kaplay.js',
   },
 ];
 
